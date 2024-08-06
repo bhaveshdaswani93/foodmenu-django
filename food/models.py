3aff85737e84fc67908f432d73c0883a85eb2e8c
Bhavesh Daswani <bhaveshdaswani93@gmail.com>
2024-08-06 14:04:23 +0530

    adding __str__ method to Item model

--- food/models.py
+++ food/models.py
@@ -5,3 +5,6 @@ class Item(models.Model):
   item_name = models.CharField(max_length=200)
   item_desc = models.TextField()
   item_price = models.FloatField()
+
+  def __str__(self):
+    return self.item_name
