f98112b3ab75f45ca98a1e37beeb4b00f7c41ab5
Bhavesh <bhaveshdaswani93@gmail.com>
2024-08-12 13:37:32 +0530

    working on static file

--- food/urls.py
+++ food/urls.py
@@ -1,6 +1,8 @@
 from . import views
 from django.urls import path
 
+app_name = 'food'
+
 urlpatterns = [
   path('', views.index, name='index'),
   path('<int:item_id>/', views.detail, name='detail'),
