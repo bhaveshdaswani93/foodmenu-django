ae1bbbeaa4e7b9b1c84210846a7abd95eed520ef
Bhavesh Daswani <bhaveshdaswani93@gmail.com>
2024-08-13 21:35:43 +0530

    working on integrating base template with food list and food detail page

--- food/models.py
+++ food/models.py
@@ -5,6 +5,7 @@ class Item(models.Model):
   item_name = models.CharField(max_length=200)
   item_desc = models.TextField()
   item_price = models.FloatField()
+  item_image = models.CharField(max_length=500, default='https://via.placeholder.com/150')
 
   def __str__(self):
     return self.item_name
